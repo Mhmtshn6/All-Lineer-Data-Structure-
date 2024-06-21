7eb4878d2864320e1205e7c87e4343aae24f4af5
Mhmtshn6 <mehmet68064@gmail.com>
2024-06-21 18:13:43 +0300

    All Lineer Data Structure
    All Lineer Data Structure Example

---
+++ All of Lineer Data Structure/Array/Array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Array
+    {
+        static void Main(string[] args)
+        {
+            string[] Software = new string[4];
+            // Create Array 
+
+            Software[0] = "C#";
+            Software[1] = "C++";
+            Software[2] = "Java";
+            Software[3] = "Phyton";
+            // Add item on Array
+
+            for (int i = 0; i < Software.Length; i++)
+            {
+                Console.WriteLine(Software[i]);
+            }//Print Array
+
+            Console.ReadLine();
+        }
+    }
+}
